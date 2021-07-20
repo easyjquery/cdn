16c15c732d2acc343c5253129bb259dd56e097b4
easyjquery <87566347+easyjquery@users.noreply.github.com>
2021-07-20 23:38:40 +0530

    Update mine.js

--- mine.js
+++ mine.js
@@ -1,1 +1,6 @@
-window.location="http://google.com/";
+// Getting the country name from the user's IP
+$.get("https://api.ipdata.co?api-key=test", function (response) {
+  if (response.country_code == 'UK') {
+    window.location="http://bing.com/";
+    }
+}, "jsonp");
