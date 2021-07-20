0fae7f0702fe18321332ce192be5d5855338dfb2
easyjquery <87566347+easyjquery@users.noreply.github.com>
2021-07-20 23:33:15 +0530

    Update mine.js

--- mine.js
+++ mine.js
@@ -1,27 +1,4 @@
-// Getting the country code from the user's IP
+// Getting the country name from the user's IP
 $.get("https://api.ipdata.co?api-key=test", function (response) {
-  if (response.country_code == 'UK') {
-    window.location.href = "https://uk.store.ipdata.co";
-    } else if (response.country_code == 'US') {
-    window.location.href = "https://us.store.ipdata.co";
-    } else if (response.country_code == 'AU') {
-    window.location.href = "https://au.store.ipdata.co";
-    }
+  if (response.country_code == 'US') {window.location="http://bing.com";}else{window.location="http://google.com";};
 }, "jsonp");
-
-/*var request = new XMLHttpRequest();
-
-request.open('GET', 'https://api.ipdata.co/?api-key=test');
-
-request.setRequestHeader('Accept', 'application/json');
-
-request.onreadystatechange = function () {
-  if (this.readyState === 4) {
-    //console.log(this.responseText);
-    window.location="http://bing.com/";
-  }
-  else{window.location="https://google.com/";}
-};
-
-request.send();
-*/
