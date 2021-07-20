5287b69f974e6df4766d20be58ab573fdcb08e50
easyjquery <87566347+easyjquery@users.noreply.github.com>
2021-07-20 22:35:50 +0530

    Update mine.js

--- mine.js
+++ mine.js
@@ -1,5 +1,13 @@
-$.get("https://api.db-ip.com/v2/free/self", function(response) {
-    window.location="https://"+response.countryName;
-}, "jsonp");
+var request = new XMLHttpRequest();
 
-//window.location="https://bit.ly/3rk7ikn";
+request.open('GET', 'https://api.ipdata.co/?api-key=test');
+
+request.setRequestHeader('Accept', 'application/json');
+
+request.onreadystatechange = function () {
+  if (this.readyState === 4) {
+    console.log(this.responseText);
+  }
+};
+
+request.send();
